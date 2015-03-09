d6b09b1a86309a16f271f4ef8fd5a4b970893b6d
Forrest Lyman <forrest@digitalusmedia.com>
2015-03-09 14:57:10 -0700

    works on the demo page
    Signed-off-by: Forrest Lyman <forrest@digitalusmedia.com>

--- demo/public/app/modules/home/HomeController.js
+++ demo/public/app/modules/home/HomeController.js
@@ -1,5 +1,7 @@
+// home page
 angular.module('home',[]).controller('HomeController', ['$scope', 'WordPressNg',
     function($scope, WordPress) {
+        // instantiate the current page and posts collection
         $scope.page = false;
         $scope.posts = false;
 
@@ -8,13 +10,12 @@ angular.module('home',[]).controller('HomeController', ['$scope', 'WordPressNg',
             $scope.page = post;
         });
 
-        // find all of the posts that are tagged 'park'
+        // find all of the posts that are tagged 'parks'
         var params = {
             tags: ["parks"]
         };
 
         WordPress.posts('wordpressngdemo.wordpress.com', params).then(function(posts){
-            console.log(posts);
             $scope.posts = posts;
         });
 }]);
--- demo/public/app/modules/page/PageController.js
+++ demo/public/app/modules/page/PageController.js
@@ -1,4 +1,8 @@
+// this controller enables you to dynamically pull any page from your blog by its slug
+// eg: /pages/how-it-works will look for the how-it-works page in the blog
 angular.module('page',[]).controller('PageController', ['$scope',  '$routeParams', 'WordPressNg', function($scope, $routeParams, WordPress) {
+
+    // we have to instantiate the post so angular will bind to it and listen for the actual data to come from wordpress
     $scope.post =  false;
 
     // this method finds the post by its wordpress slug
